679d8742be76fb022542624240c29e7ea18ec086
Laura Saavedra <laurac.saavedra@udea.edu.co>
2026-06-02 09:21:03 -0500

    Delete src directory

--- src/main/java/com/panaderia/repository/ClienteRepository.java
+++
@@ -1,7 +0,0 @@
-package com.panaderia.repository;
-
-import com.panaderia.model.Cliente;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ClienteRepository extends JpaRepository<Cliente, Long> {
-}
